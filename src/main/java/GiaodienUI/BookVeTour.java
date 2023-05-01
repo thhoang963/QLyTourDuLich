db6dde09be26168bdf923d9ed18f00cbf0d8d600
ThanhTran2107 <thanhcnttmcpe@gmail.com>
2023-05-01 08:54:15 +0700

    update

--- src/main/java/GiaodienUI/BookVeTour.java
+++ src/main/java/GiaodienUI/BookVeTour.java
@@ -59,7 +59,7 @@ public class BookVeTour extends javax.swing.JPanel {
         jLabel1.setBackground(new java.awt.Color(255, 255, 255));
         jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(255, 0, 0));
-        jLabel1.setText("                Hệ Thống Bán Vé Tour Du Lịch Toàn Quốc ");
+        jLabel1.setText("                Hệ Thống Đặt Vé Tour Du Lịch Toàn Quốc ");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
